4030a507af25f22ad21805d5858ee29d07254e7f
coltonsr77 <coltonr3@icloud.com>
2025-10-06 16:50:44 -0400

    Delete OOBE-MOD-DATA/Tool#1.bat

--- OOBE-MOD-DATA/Tool#1.bat
+++
@@ -1,7 +0,0 @@
-echo Welcome to the OOBE Mod V1.0 by Coltonsr77
-pause
-echo Checking the OOBE-MOD-TOOLS and OOBE-MOD-DATA folder right now.
-pause
-echo Launching OOBE_MOD.bat right now.
-OOBE-MOD-DATA/OOBE_MOD.bat
-exit
