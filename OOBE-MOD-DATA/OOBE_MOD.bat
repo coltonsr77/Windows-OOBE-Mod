335d714faaa260b3eb64c5eb57189bc02ceddff8
coltonsr77 <coltonr3@icloud.com>
2025-10-06 16:11:59 -0400

    1.0 TESTING

---
+++ OOBE-MOD-DATA/OOBE_MOD.bat
@@ -0,0 +1,22 @@
+echo OOBE MOD by Coltonsr77 V1.0-TESTING
+pause
+echo Setting OOBE up.
+pause
+echo Done.
+pause
+echo Step 1: Installing Patches
+pause
+OOBE_BYPASSES.reg
+echo Done.
+pause
+echo Step 2: Running Tool: NPCAP
+pause
+NPCAP_OOBE.exe
+pause
+echo Done.
+pause
+echo Step 3: Finishing Touchs
+pause
+echo Restarting system now.
+pause
+restart
