07120571a00dde8da4dcb15792d892c6bdc706d4
coltonsr77 <coltonr3@icloud.com>
2025-12-02 17:00:17 -0500

    Update OOBE_MOD.bat

--- OOBE-MOD-DATA/OOBE_MOD.bat
+++ OOBE-MOD-DATA/OOBE_MOD.bat
@@ -6,7 +6,7 @@ echo Make sure to check for updates on GitHub.
 pause
 echo Step 1: Downloading Patches
 pause
-echo Run Download_Patches.bat in OOBE-MOD-TOOLS to download the patch installer. (V1.2 can't run the file till V1.3)
+echo Run Download_Patches.bat in OOBE-MOD-TOOLS to download the patch installer. (OOBE can't run the file till V1.3)
 pause
 cls
 echo Step 2: Installing Tool: Npcap
