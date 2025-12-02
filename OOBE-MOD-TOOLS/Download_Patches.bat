b9844bc63119fb0a9b512d8501ccdca33b15c880
coltonsr77 <coltonr3@icloud.com>
2025-12-02 16:10:18 -0500

    Update Download_Patches.bat

--- OOBE-MOD-TOOLS/Download_Patches.bat
+++ OOBE-MOD-TOOLS/Download_Patches.bat
@@ -1,1 +1,16 @@
+cls
+@ECHO OFF
 
+:choice
+set /P c=Do you want to download all patches[Y/N]?
+if /I "%c%" EQU "Y" goto :somewhere
+if /I "%c%" EQU "N" goto :somewhere_else
+goto :choice
+
+:somewhere
+
+
+
+:somewhere_else
+exit
+@ECHO OFF
