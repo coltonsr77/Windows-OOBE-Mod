3d8298b6b78550fc39efa4d591251c9f7ffd502e
coltonsr77 <coltonr3@icloud.com>
2025-12-01 21:05:07 -0500

    Update OOBE_MOD.bat

--- OOBE-MOD-DATA/OOBE_MOD.bat
+++ OOBE-MOD-DATA/OOBE_MOD.bat
@@ -6,6 +6,7 @@ echo Step 1: Downloading Patches
 pause
 echo Download the patches from https://github.com/coltonsr77/Windows-Setup-Patchs/releases/latest
 pause
+echo (If you can't use the link use Download_Patches.bat in OOBE-MOD-TOOLS)
 echo Step 2: Installing Tool: Npcap
 pause
 start npcap-1.85.exe
